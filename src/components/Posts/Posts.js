67a198ab9cfce24be84fd2abce841e5942b87b4a
Your Name <you@example.com>
2023-12-13 10:35:45 +0600

    Toast Added for Success and Fail Status

--- src/components/Posts/Posts.js
+++ src/components/Posts/Posts.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Post from '../Post/Post';
 import Bookmark from '../Bookmark/Bookmark';
+import toast, { Toaster } from 'react-hot-toast';
 
 const Posts = () => {
     const [posts, setPosts] = useState([]);
@@ -13,20 +14,37 @@ const Posts = () => {
             .then(data => setPosts(data))
     }, []);
 
-    // Add Bookmark Event handler
-    const addBookmark = posts => {
-        const newBookmarks = [...bookmarks, posts];
-        setBookmarks(newBookmarks);
-    }
+    // toaster Added
+    const successNotify = () => toast.success('Successfully Added!');
+    const errorNotify = () => toast.error('Already Added!');
 
+
+    // Add Bookmark Event handler
+    const addBookmark = bookmarkPosts => {
+        
+        const exits = bookmarks.find(post => post.id === bookmarkPosts.id);
+
+        if(exits){
+            errorNotify();
+            return;
+        }
+        successNotify();
+
+        const newBookmarks = [...bookmarks, bookmarkPosts];
+        setBookmarks(newBookmarks);
+        
+    }
+    
     // Mark as Read Function Event Handler
     const markRead = posts => {
         const updateTime = posts.readTime + readTime;
         setReadTime(updateTime);
+        successNotify();
     }
-    console.log(readTime);
+
     return (
         <div className='container mx-auto px-8'>
+             <Toaster />
             <div className='grid grid-cols-1 lg:grid-cols-[2fr_1fr] gap-6 py-8'>
                 <div className='flex flex-col gap-10'>
                     {
